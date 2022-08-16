fc82de29827472ce86bd47578d233f6249bc9498
Rohesa123 <rohesasidiqpermana05@gmail.com>
2022-08-16 18:33:15 +0700

    Memperbaiki model wali

--- app/Models/Wali.php
+++ app/Models/Wali.php
@@ -18,17 +18,17 @@ class Wali extends Model
 
     public function image() 
     {
-        if ($this->foto && file_exists(public_path('images/wali/' . $this->foto))) {
-            return asset('images/wali/' . $this->foto);
+        if ($this->foto_produk && file_exists(public_path('images/produk/' . $this->foto_produk))) {
+            return asset('images/produk/' . $this->foto_produk);
         } else {
-            return asset('images/no_image.jpg');
+            return asset('images/produk/no_image.jpg');
         }
     }
 
     public function deleteImage()
     {
-        if ($this->foto && file_exists(public_path('images/wali/'.$this->foto))) {
-            return unlink(public_path('images/wali/'.$this->foto));
+        if ($this->foto_produk && file_exists(public_path('images/wali/'.$this->foto_produk))) {
+            return unlink(public_path('images/produk/'.$this->foto_produk));
         }
     }
 }
